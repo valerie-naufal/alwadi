5e21069706435c0af21fcef1daebcef8b5de3ae9
valerie naufal <valerie.naufal@gmail.com>
2024-07-17 16:26:19 +0300

    Recipes page - In progress

--- scripts/script.js
+++ scripts/script.js
@@ -53,5 +53,41 @@ $(document).ready(function () {
     },
   });
 
+  $(".owl-four").owlCarousel({
+    items: 1,
+    nav: true,
+    navText: ["<", ">"],
+    dots: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 3,
+      },
+      1000: {
+        items: 4,
+      },
+    },
+  });
+
+  $(".owl-five").owlCarousel({
+    items: 1,
+    nav: true,
+    navText: ["<", ">"],
+    dots: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 2,
+      },
+      1000: {
+        items: 3,
+      },
+    },
+  });
+
   $(".tooltip").tooltip();
 });
